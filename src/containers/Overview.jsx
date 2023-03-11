d6b7512ab6de6df712acb5359531dc2997fb430e
YAKSHIT-22 <yakshitgarg25@gmail.com>
2023-03-11 10:48:52 +0530

    minor fix

--- src/containers/Overview.jsx
+++ src/containers/Overview.jsx
@@ -9,7 +9,7 @@ const Overview = () => {
                    <div className="flex items-center justify-center max-w-6xl p-2 pb-0 md:p-4 md:pb-0">
                            <div className="flex items-center justify-center sm:p-4 pb-0 sm:pb-0 relative">
                             <div className="flex items-center justify-center relative p-2 pb-0 s:p-4 s:pb-0 sm:p-6 sm:pb-0">
-                                <img src={bg} alt="bg" className="w-full h-full object-cover rounded-t-[56px] xs:rounded-t-[72px] sm:rounded-t-[104px] md:rounded-t-[112px]"/>
+                                <img src={bg} alt="bg" className="w-full h-full object-cover rounded-t-[56px] xs:rounded-t-[64px] sm:rounded-t-[104px] md:rounded-t-[112px]"/>
                                 <img src={play} alt="play" className="h-14 w-14 xs:w-16 xs:h-16 s:h-20 s:w-20  sm:w-24 sm:h-24 lg:w-28 lg:h-28 absolute top-0 right-0"/>
                                 </div>
                            </div>
