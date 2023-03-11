9c0ff3df41f1602e0a80a24a9fc3c845ebf5478a
YAKSHIT-22 <yakshitgarg25@gmail.com>
2023-03-12 04:45:27 +0530

    additions

--- src/containers/Prizes.jsx
+++ src/containers/Prizes.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Prizes = () => {
   return (
-    <section className="w-screen h-screen bg-black">Prizes</section>
+    <section className="w-screen h-screen bg-black">
+      <div className="w-full h-full flex items-center justify-center">
+        <div className="w-full h-full flex items-center justify-center flex-row">
+          <div className="w-full h-full flex items-start justify-start flex-col p-6">
+          <div className="flex items-center justify-start">
+          <h1 className='text-[2rem]  lg:p-2 xs:text-[2.7rem] s:text-5xl sm:text-[4rem] md:text-[5rem] lg:text-8xl xl:text-9xl 2xl:text-[154px] font-black sm:leading-tight text-[#ffffff]'>PRIZES <span className="text-[#d5FF40]">.</span></h1>
+          </div>
+          <div className='xs:flex items-center justify-center flex-row w-full sm:w-[75%] md:w-[65%] lg:w-[50%] sm:p-2 my-6'>
+                        <p className='font-normal text-xs text-white sm:text-sm lg:text-lg '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+              </div>
+          </div>
+          <div className="lg:flex hidden items-center justify-center">
+
+          </div>
+          </div>
+        </div>
+    
+    </section>
   )
 }
 
